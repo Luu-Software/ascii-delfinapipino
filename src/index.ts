0bcce77e1f6ea14ce344eac2e0e4565001e0309a
delfinapipino <delfinasolp@gmail.com>
2026-06-17 15:21:24 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,8 @@ function repetirLetra(letra: string, cantidad: number): string {
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
+let contador= 
+while (contador==);
+  console.log ('')
 
 // COMPLETAR
