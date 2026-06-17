8ef60f07eefec013bf86fc3a6b558e8c937594ad
delfinapipino <delfinasolp@gmail.com>
2026-06-17 15:54:15 -0300

    terminado brooo

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import { preguntar } from './lib/consola.ts';
 
 function repetirLetra(letra: string, cantidad: number): string {
-  // COMPLETAR
-}
+ let resultado ='';
+
+ while(cantidad>0){
+    resultado += letra;
+    cantidad = cantidad - 1;
+  }
+  return resultado;
+ }
+
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
-let contador= 
-while (contador==);
-  console.log ('')
+
+let filasRestantes: number = tamaño;
+
+while (filasRestantes > 0) {
+  console.log(repetirLetra(caracter, tamaño));
+  filasRestantes = filasRestantes - 1;
+}
+
 
 // COMPLETAR
